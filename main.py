2eb7d82280861d625fb7bb39d83f64dabea0ef36
ashujohri <ashutoshjohri5@gmail.com>
2026-06-28 01:49:17 +0530

    create schemas file for add tempporary post

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -36,7 +37,7 @@ def home(request: Request):
     return templates.TemplateResponse(request, "home.html",  {"posts": posts})
 
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse], name="api_posts")
 def get_posts():
     return posts
 
@@ -47,12 +48,25 @@ def post_page(request: Request, post_id: int):
             return templates.TemplateResponse(request, "post.html", {"post": post}, status_code=status.HTTP_200_OK)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
-@app.get("/api/posts/{post_id}")
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_post(request: Request, post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             return templates.TemplateResponse(request, "post.html", {"post": post}, status_code=status.HTTP_200_OK)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+@app.post("/api/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 22, 2025",
+    }
+    posts.append(new_post)
+    return new_post
 
 @app.exception_handler(StarletteHTTPException)
 def general_http_exception_handler(request: Request, exc: StarletteHTTPException):
