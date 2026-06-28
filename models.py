6d8ea7baee092f5bd2b2e973c25b81aaa35f7891
ashujohri <ashutoshjohri5@gmail.com>
2026-06-29 01:10:10 +0530

    adding user route/api and user post page

---
+++ models.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from datetime import UTC, datetime
+from sqlalchemy import Column, DateTime, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, index=True, nullable=False)
+    image_file: Mapped[str | None] = mapped_column(String(200), nullable=True, default=None)
+    posts: Mapped[list[Post]] = relationship(back_populates="author")
+    
+    @property
+    def image_path(self) -> str:
+        if self.image_file:
+            return f"/media/profile_pics/{self.image_file}"
+        return "/static/profile_pics/default.jpg"
+
+class Post(Base):
+    __tablename__ = "posts"
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False, index=True)
+    author: Mapped[User] = relationship(back_populates="posts")
+    date_posted: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda:datetime.now(UTC), nullable=False)
--- schemas.py
+++ schemas.py
@@ -1,15 +1,29 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from datetime import datetime
 
+class UserBase(BaseModel):
+    username: str = Field(..., min_length=1, max_length=50)
+    email: EmailStr
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id: int
+    image_path: str
+    image_file: str | None = None
 class PostBase(BaseModel):
     title: str = Field(..., min_length=1, max_length=100)
     content: str = Field(..., min_length=1, max_length=1000)
-    author: str = Field(..., min_length=1, max_length=50)
 
 class PostCreate(PostBase):
-    pass
+    user_id: int  # TEMPORARY
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     
     id: int
-    date_posted: str
+    date_posted: datetime
+    author: UserResponse
