3c040c090b9a9f1f8a843f1464655eba54058a9e
ashujohri <ashutoshjohri5@gmail.com>
2026-06-29 02:08:35 +0530

    define update and delete routes for user and posts

--- schemas.py
+++ schemas.py
@@ -14,12 +14,21 @@ class UserResponse(UserBase):
     id: int
     image_path: str
     image_file: str | None = None
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None, max_length=200, min_length=1)
 class PostBase(BaseModel):
     title: str = Field(..., min_length=1, max_length=100)
     content: str = Field(..., min_length=1, max_length=1000)
 
 class PostCreate(PostBase):
     user_id: int  # TEMPORARY
+
+class PostUpdate(BaseModel):
+    title: str | None = Field(default=None, min_length=1, max_length=100)
+    content: str | None = Field(default=None, min_length=1, max_length=1000)
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
